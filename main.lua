697310309a217324b1335697a1b7eafacc177c40
Esubishi <130168612+Esubishi@users.noreply.github.com>
2023-06-27 13:07:35 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -11,16 +11,19 @@ local Char = Player.Character
 local H_R_P = Char:WaitForChild("HumanoidRootPart")
 local bp = Instance.new("BodyPosition", H_R_P)
 local bg = Instance.new("BodyGyro", H_R_P)
+print("Variables loaded")
 --Fly things
 bp.MaxForce = Vector3.new()
 bp.D = 10
 bp.P = 10000
 bg.MaxTorque = Vector3.new()
 bg.D = 10
+print("Fly things loaded")
 --Screen Gui
 local Wind = Instance.new("ScreenGui")
 Wind.Name = "Wind"
 Wind.Parent = game.CoreGui
+print("ScreenGui loded")
 --Frame
 local Frame = Instance.new("Frame")
 local f_ui = Instance.new("UICorner")
@@ -35,17 +38,23 @@ Frame.Position = UDim2.new(0.5, 0, 0.5, 0)
 Frame.AnchorPoint = Vector2.new(0.5, 0.5)
 Frame.BackgroundColor3 = Color3.fromRGB(77, 62, 82)
 Frame.BorderSizePixel = 0
+print("Frame loaded")
 --Open/Close Button
 local O_C = false
 local O_C_B = Instance.new("TextButton")
+local O_C_B_ui = Instance.new("UICorner")
+O_C_B_ui.CornerRadius = UDim.new(0,32)
+O_C_B_ui.Parent = O_C_B
 O_C_B.Parent = Wind
 O_C_B.BorderSizePixel = 0
-O_C_B.BackgroundColor3 = Color3.fromRGB(168, 28, 214)
+O_C_B.BackgroundColor3 = Color3.fromRGB(196, 150, 212)
 O_C_B.Name = "Open_Close"
 O_C_B.Text = "Open or Close"
-O_C_B.Size = UDim2.new(0, 100, 0, 100)
-O_C_B.Position = UDim2.new(0.1, 0, 0.4, 0)
-O_C_B.TextSize = 12
+O_C_B.Size = UDim2.new(0, 200, 0, 100)
+O_C_B.Position = UDim2.new(0, 0, 0.8, 0)
+O_C_B.TextColor3 = Color3.fromRGB(255,255,255)
+O_C_B.FontFace.Weight = Enum.FontWeight.Bold
+O_C_B.TextSize = 15
 O_C_B.Draggable = true
 O_C_B.MouseButton1Click:Connect(function()
 	if O_C == false then
@@ -56,9 +65,13 @@ O_C_B.MouseButton1Click:Connect(function()
 		O_C = false
 	end
 end)
+print("OCB laoded")
 --Walkspeed changerese
+local wsc = Instance.new("UICorner")
 local ws_c = Instance.new("TextBox")
 ws_c.Name = "Walkspeed changer"
+wsc.Parent = ws_c
+wsc.CornerRadius = UDim.new(0,32)
 ws_c.Size = UDim2.new(0, 100, 0, 50)
 ws_c.Parent = Frame
 ws_c.Position = UDim2.new(0.1, 0, 0.4, 0)
@@ -72,9 +85,13 @@ ws_c.FocusLost:Connect(function(enter)
 		print("A mers domnule"..Player.Name)
 	end
 end)
+print("Walkspeed changer loaded")
 --Change Walkspeed Label
+local wsl = Instance.new("UICorner")
 local ws_l = Instance.new("TextButton")
 local ws = false
+wsl.Parent = ws_l
+wsl.CornerRadius = UDim.new(0,32)
 ws_l.Name = "Walspeed"
 ws_l.Text = "More Walkspeed"
 ws_l.Parent = Frame
@@ -97,12 +114,16 @@ ws_l.MouseButton1Click:Connect(function()
 		ws = false
 	end
 end)
+print("Walkspeed loaded")
 --Flyspeed changer
+local fsc_ui = Instance.new("UICorner")
 local fs_c = Instance.new("TextBox")
+fsc_ui.Parent = fs_c
+fsc_ui.CornerRadius = UDim.new(0,32)
 fs_c.Name = "Flyspeed changer"
 fs_c.Size = UDim2.new(0, 100, 0, 50)
 fs_c.Parent = Frame
-fs_c.Position = UDim2.new(0.4, 0, 0.4, 0)
+fs_c.Position = UDim2.new(0.735, 0, 0.4, 0)
 fs_c.BackgroundColor3 = Color3.fromRGB(196, 150, 212)
 fs_c.BorderSizePixel = 0
 fs_c.Text = "Enter the speed"
@@ -113,13 +134,17 @@ fs_c.FocusLost:Connect(function(enter)
 		print("A mers domnule"..Player.Name)
 	end
 end)
+print("Flyspeed changer loaded")
 --Fly Label
+local fly_ui = Instance.new("UICorner")
 local f_l = Instance.new("TextButton")
 local flying = false
+fly_ui.Parent = f_l
+fly_ui.CornerRadius = UDim.new(0, 32)
 f_l.Parent = Frame
 f_l.Name = "Fly"
 f_l.Text = "Fly"
-f_l.Position = UDim2.new(0.4, 0, 0.1, 0)
+f_l.Position = UDim2.new(0.65, 0, 0.1, 0)
 f_l.Size = UDim2.new(0, 150, 0, 100)
 f_l.BackgroundColor3 = Color3.fromRGB(196, 150, 212)
 f_l.BorderSizePixel = 0
@@ -142,20 +167,15 @@ f_l.MouseButton1Click:Connect(function()
 		 flying = false
 	 end
 end)
---Noclip gui
-local nogui = Instance.new("TextButton")
-nogui.Parent = Wind
-nogui.Name = "Noclip"
-nogui.Text = "off"
-nogui.Size = UDim2.new(0, 150, 0, 100)
-nogui.Position = UDim2.new(0.1, 0, 0.5, 0)
-nogui.BorderSizePixel = 0
-nogui.BackgroundColor3 = Color3.fromRGB(168, 28, 214)
-nogui.Visible = false
+print("Fly loaded")
 --Noclip
+local noclip_ui = Instance.new("UICorner")
 local noct = false
 local noclip = false
 local noc = Instance.new("TextButton")
+local helper_noclip = false
+noclip_ui.CornerRadius = UDim.new(0,32)
+noclip_ui.Parent = noc
 noc.Parent = Frame
 noc.Text = "Noclip"
 noc.Name = "Esp"
@@ -165,34 +185,27 @@ noc.BackgroundColor3 = Color3.fromRGB(196, 150, 212)
 noc.BorderSizePixel = 0
 noc.MouseButton1Click:Connect(function()
 	if noct == false then
+		noclip = true
 		noct = true
 		noc.BackgroundColor3 = Color3.fromRGB(106, 91, 235)
-		nogui.Visible = true
-		if nogui.Text == "off" then
-			noclip = true
-			nogui.Text = "on"
-			Stepped = rs.Stepped:Connect(function ()
-				if not noclip == false then
-					for a,b in pairs(Workspace:GetChildren()) do
-						if b.Name == Player.Name then
-							for i,v in pairs(Workspace[Player.Name]:GetChildren()) do
-								if v:isA("BasePart") then
-									v.CanCollide = false
-								end
+		Stepped = rs.Stepped:Connect(function ()
+			if noclip == true then
+				for a,b in pairs(Workspace:GetChildren()) do
+					if b.Name == Player.Name then
+						for i,v in pairs(Workspace[Player.Name]:GetChildren()) do
+							if v:isA("BasePart") then
+								v.CanCollide = false
 							end
 						end
 					end
-				else
-					Stepped:Disconnect()
 				end
-			end)
-		elseif nogui.Text == "on" then
-			noclip = false
-			nogui.Text = "off"
-		end
+			else
+				Stepped:Disconnect()
+			end
+		end)
 	elseif noct == true then
 		noc.BackgroundColor3 = Color3.fromRGB(196, 150, 212)
 		noct = false
-		nogui.Visible = false
+		noclip = false
 	end
 end)
