5e12514e6304de66795988b5d14861b2fadd99d8
Esubishi <130168612+Esubishi@users.noreply.github.com>
2024-05-06 11:30:23 +0300

    update for main frame

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
 --ImportantVariabiles
 local userInputService = game:WaitForChild("UserInputService")
 local player = game.Players.LocalPlayer
-local OpenFrame = false
 --Functions
 local function run(func) func() end
 --BaseGUI
@@ -14,22 +13,21 @@ local B_FrameCornerUI = Instance.new("UICorner")
 B_FrameCornerUI.Parent = B_Frame
 B_FrameCornerUI.CornerRadius = UDim.new(0.1,0)
 B_Frame.Parent = CoreGUI
-B_Frame.Visible = false
 B_Frame.Name = "BaseFrame"
 B_Frame.Size = UDim2.new(0.3,0,0.7,0)
 B_Frame.BackgroundColor3 = Color3.fromRGB(24,193,255)
 B_Frame.Draggable = true
+B_Frame.Position = UDim2.new(0.349,0,0.149)
 
-userInputService.InputBegan:Connect(function(input, _gameProcessed)
-	if _gameProcessed then
-		if input.KeyCode == Enum.KeyCode.P then
-			if OpenFrame == false then
-				OpenFrame = true
-				B_Frame.Visible = true
-			elseif OpenFrame == true then
-				OpenFrame = false
-				B_Frame.Visible = false
-			end
+userInputService.InputBegan:Connect(function(input)
+	local openFrame = false
+	if input.KeyCode == Enum.KeyCode.P then
+		if openFrame == false then
+			openFrame = true
+			B_Frame.Visible = true
+		elseif openFrame == true then
+			openFrame = false
+			B_Frame.Visible = false			
 		end
 	end
 end)
