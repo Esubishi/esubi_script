8d304382ad12907b2914e9834f69e6de2b4e051e
Esubishi <130168612+Esubishi@users.noreply.github.com>
2024-05-06 12:17:09 +0300

    working hahahaha

--- main.lua
+++ main.lua
@@ -1,6 +1,8 @@
 --ImportantVariabiles
 local userInputService = game:WaitForChild("UserInputService")
 local player = game.Players.LocalPlayer
+local open = false
+
 --Functions
 local function run(func) func() end
 --BaseGUI
@@ -14,20 +16,24 @@ B_FrameCornerUI.Parent = B_Frame
 B_FrameCornerUI.CornerRadius = UDim.new(0.1,0)
 B_Frame.Parent = CoreGUI
 B_Frame.Name = "BaseFrame"
+B_Frame.Visible = false
 B_Frame.Size = UDim2.new(0.3,0,0.7,0)
 B_Frame.BackgroundColor3 = Color3.fromRGB(24,193,255)
 B_Frame.Draggable = true
 B_Frame.Position = UDim2.new(0.349,0,0.149)
-
-userInputService.InputBegan:Connect(function(input)
-	local openFrame = false
-	if input.KeyCode == Enum.KeyCode.P then
-		if openFrame == false then
-			openFrame = true
-			B_Frame.Visible = true
-		elseif openFrame == true then
-			openFrame = false
-			B_Frame.Visible = false			
+--Gui function
+local function openFrame(input,_gameProcessed)
+	if input.UserInputType == Enum.UserInputType.Keyboard then
+		if input.KeyCode == Enum.KeyCode.P then
+			if open == false then
+				open = true
+				B_Frame.Visible = true
+			else
+				open = false
+				B_Frame.Visible = false
+			end
 		end
 	end
-end)
+end
+
+userInputService.InputBegan:Connect(openFrame)
