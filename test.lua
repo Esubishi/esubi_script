6a78fa67a220701866b968982f8b6bf2fd072981
Esubishi <130168612+Esubishi@users.noreply.github.com>
2025-01-18 20:46:51 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,8 +1,6 @@
 --ImportantVariabiles
 local userInputService = game:WaitForChild("UserInputService")
 local player = game.Players.LocalPlayer
-local currentWalkspeed = player.Character.Humanoid.WalkSpeed
-
 --BaseGUI
 local CoreGUI = Instance.new("ScreenGui")
 CoreGUI.Parent = game.CoreGui
@@ -15,53 +13,51 @@ MainFrame.BackgroundColor3 = Color3.fromRGB(197,196,196)
 MainFrame.BackgroundTransparency = 0.75
 MainFrame.Size = UDim2.new(1,0,1,0)
 MainFrame.Visible = true
+--Test
+
+
 --Functions
 
-local function CreateButton(text, parent, backgorund_color, text_color, isDraggable,button_type)
-    local button = Instance.new(button_type)
+local function CreateButton(text, parent, isDraggable,button_type,size)
+	--Error checking
+	if button_type ~= "TextButton" and button_type ~= "TextLabel" then
+		print("Button Creation Error: '"..button_type.."' is not a valid button type")
+		return -1
+	end
+	--Variabiles
+	local button = Instance.new(button_type)
 	--Button Config
 	button.Name = "Button"
-    button.Text = text
-    button.Parent = parent
-	button.Size = UDim2.new(0.2,0,0.1,0)
+	button.Text = text
+	button.Parent = parent
+	button.Size = UDim2.new(tonumber(size))
 	button.BorderSizePixel = 0
-    button.Font = "FredokaOne"
+	button.Font = "FredokaOne"
 	button.TextScaled = true
 	button.Draggable = isDraggable
-	--Color check
-	if backgorund_color == nil then
-        button.BackgroundColor3 = Color3.fromRGB(255,255,255)
-    elseif backgorund_color ~= nil then
-        button.BackgroundColor3 = Color3.fromRGB(tonumber(backgorund_color))
-    elseif text_color == nil then
-        button.TextColor3 = Color3.fromRGB(0,0,0)
-    elseif text_color ~= nil then
-        button.TextColor3 = Color3.fromRGB(tonumber(text_color))
-    end
-    return button
+	return button
 end
 
 local function CreateTab(name,parent)
 	--Variabiles
-	local tab = Instance.new("ScrollingFrame")
+	local tab = Instance.new("Frame")
 	local Name_label = Instance.new("TextLabel")
 	--Label
-	
+	Name_label.Name = name
+	Name_label.Text = " "
+	Name_label.Parent = parent
+	Name_label.Size = UDim2.new(0.13,0,0.5,0)
+	Name_label.Draggable = true
 	--Tab
 	tab.Parent = Name_label
 	tab.Name = name
 	tab.BorderSizePixel = 0
 	tab.Visible = true
-	tab.Size = UDim2.new(0.13,0,0.3,0)
-	tab.Visible = false
+	tab.Size = UDim2.new(1,0,1,0)
 	--Return	
 	return tab
 end
 
 local MovmentTab = CreateTab("Movment", MainFrame)
 
-local button = CreateButton("TEST",MainFrame,nil,nil,true,"TextButton")
-
-button.MouseButton1Click:Connect(function()
-	print("Yea")
-end)
+local button = CreateButton("TEST",MovmentTab,false,"TextButton","1,0,1,0")
