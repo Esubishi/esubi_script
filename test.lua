829c6b5da1d8b049019d50fb7ee546186fff39b2
Esubishi <130168612+Esubishi@users.noreply.github.com>
2025-01-19 17:51:42 +0200

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,6 +1,8 @@
 --ImportantVariabiles
 local userInputService = game:WaitForChild("UserInputService")
 local player = game.Players.LocalPlayer
+local TL = "TextLabel"
+local TB = "TextButton"
 --BaseGUI
 local CoreGUI = Instance.new("ScreenGui")
 CoreGUI.Parent = game.CoreGui
@@ -13,51 +15,80 @@ MainFrame.BackgroundColor3 = Color3.fromRGB(197,196,196)
 MainFrame.BackgroundTransparency = 0.75
 MainFrame.Size = UDim2.new(1,0,1,0)
 MainFrame.Visible = true
---Test
-
 
 --Functions
 
-local function CreateButton(text, parent, isDraggable,button_type,size)
-	--Error checking
-	if button_type ~= "TextButton" and button_type ~= "TextLabel" then
-		print("Button Creation Error: '"..button_type.."' is not a valid button type")
+local function setSize(scalex,offsetx,scaley,offsety)
+	return UDim2.new(scalex,offsetx,scaley,offsety)
+end
+
+local function CreateButton(conf)
+	--First hand Error checking
+	if not conf.Parent then
+		print("Button: '"..conf.Name.."' needs a parent")
 		return -1
 	end
-	--Variabiles
-	local button = Instance.new(button_type)
-	--Button Config
-	button.Name = "Button"
-	button.Text = text
-	button.Parent = parent
-	button.Size = UDim2.new(tonumber(size))
+	
+	if conf.Type ~= "TextLabel" and conf.Type ~= "TextButton" then
+		print("Error: '"..conf.Type.."' is not a valid button type")
+		return -1
+	end	
+
+	
+	
+	local button = Instance.new(conf.Type)
+	button.Parent = conf.Parent	
+	button.TextScaled = true
 	button.BorderSizePixel = 0
 	button.Font = "FredokaOne"
-	button.TextScaled = true
-	button.Draggable = isDraggable
+	button.Draggable = conf.Draggable
+	--Second hand error checking
+	
+	if not conf.Size then
+		button.Size = setSize(1,0,1,0)
+	else
+		button.Size = conf.Size
+	end
+	
+	if not conf.Name then
+		button.Name = "Button"
+	else
+		button.Name = conf.Name
+	end
+	
+	if not conf.Text then
+		button.Text = "Button"
+	else
+		button.Text = conf.Text
+	end
+	
+	if not conf.Func then	
+	else
+		button.MouseButton1Click:Connect(conf.Func)
+	end
+	
 	return button
 end
 
-local function CreateTab(name,parent)
-	--Variabiles
-	local tab = Instance.new("Frame")
-	local Name_label = Instance.new("TextLabel")
-	--Label
-	Name_label.Name = name
-	Name_label.Text = " "
-	Name_label.Parent = parent
-	Name_label.Size = UDim2.new(0.13,0,0.5,0)
-	Name_label.Draggable = true
-	--Tab
-	tab.Parent = Name_label
-	tab.Name = name
-	tab.BorderSizePixel = 0
-	tab.Visible = true
-	tab.Size = UDim2.new(1,0,1,0)
-	--Return	
-	return tab
-end
+--Test
 
-local MovmentTab = CreateTab("Movment", MainFrame)
-
-local button = CreateButton("TEST",MovmentTab,false,"TextButton","1,0,1,0")
+local Walkspeed_button = CreateButton({
+	Name = "Walkspeed",
+	Text = "Walkspeed",
+	Type = TB,
+	Parent = MainFrame,
+	isClicked = false,
+	Draggable = true,
+	Size = setSize(0.13,0,0.30,0),
+	
+	Func = function ()
+		if isClicked == false then
+			player.Character:FindFirstChildWhichIsA("Humanoid").WalkSpeed = 100
+			isClicked = true
+		else
+			player.Character:FindFirstChildWhichIsA("Humanoid").WalkSpeed = 16
+			isClicked = false
+		end
+	end
+	
+})
