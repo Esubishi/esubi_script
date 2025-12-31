dcf8bbc1160008ba0e249413be67a2b2da5e2a65
Esubishi <130168612+Esubishi@users.noreply.github.com>
2025-12-31 14:01:22 +0200

    Update Variabiles.lua

--- Variabiles.lua
+++ Variabiles.lua
@@ -1,1 +1,6 @@
-
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local RunService = game:GetService
+local Camera = workspace.CurrentCamera
+local Input = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
